2cdcf0eab2a4c9d44238b7228e4a1142b67a93ef
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-07-18 14:33:38 +0600

    done firebase connection

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: Colors.white,
+      ),
+    );
+  }
+}
